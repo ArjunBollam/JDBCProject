e7b94616ff3ade555bb60d27c5169d694609bcc2
bibek2012 <31773894+bibek2012@users.noreply.github.com>
2017-09-08 15:09:56 -0500

    adjd

--- src/com/cubic/jdbc/JDBCdemo.java
+++ src/com/cubic/jdbc/JDBCdemo.java
@@ -12,6 +12,6 @@ public class JDBCdemo {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","Arjun1234");
 		return conn;
-		//Commented by Me
+		//Commented by bibek
 	}
 }
